85969b3e7822ad45cf35d1a5839c6e83131cdbfb
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-07-02 10:35:27 +0530

    Different-Layouts-Types

---
+++ Lab_Assingments-20240702T045700Z-001/Lab_Assingments/app/src/main/java/com/example/lab_assingments/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab_assingments;
+ // Replace with your actual package name
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
