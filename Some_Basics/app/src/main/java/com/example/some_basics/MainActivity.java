fc98818737eacac908fbbcf5e13b0465170c839d
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-07-23 10:10:39 +0530

    basic_app

---
+++ Some_Basics/app/src/main/java/com/example/some_basics/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.some_basics;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            EditText obje=(EditText) findViewById(R.id.input1);
+            String u=obje.getText().toString();
+            TextView obj=(TextView)  findViewById(R.id.tv1);
+            obj.setText(u);
+
+            return insets;
+        });
+    }
+}
