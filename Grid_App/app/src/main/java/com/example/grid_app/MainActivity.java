9e4dc2b4ac231de472ba61c5de00e4eca62c8cd4
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-08-08 15:09:27 +0530

    grid_app

---
+++ Grid_App/app/src/main/java/com/example/grid_app/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.grid_app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;//class required to adapt the view
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            GridView grid=findViewById(R.id.grid);
+            //create an array that contains the item to be displayed
+            String [] item={"Java","Python","R","Android"};
+
+            //CREATE AN ARRAY ADAPTER
+            ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,item);
+            //bind adapter to gridview
+            grid.setAdapter(adapter);
+
+            grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String item=adapter.getItem(i);
+                    Toast.makeText(MainActivity.this,"you Selected :"+item,Toast.LENGTH_SHORT).show();
+                }
+            });
+            return insets;
+        });
+    }
+}
