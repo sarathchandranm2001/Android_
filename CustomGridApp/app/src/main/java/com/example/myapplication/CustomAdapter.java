825c2b37e9265c2a78343501edd8a0ec35e3a088
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-08-09 01:06:04 +0530

    custm adptr

--- CustomGridApp/app/src/main/java/com/example/myapplication/CustomAdapter.java
+++ CustomGridApp/app/src/main/java/com/example/myapplication/CustomAdapter.java
@@ -3,16 +3,29 @@ package com.example.myapplication;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.content.Context;
+import android.view.LayoutInflater;
 public class CustomAdapter extends BaseAdapter {
-    @Override
-    public int getCount() {
-        return 0;
+    private Context context;
+    private final String[] items;
+    private final int[] ImageIds;
+
+    public CustomAdapter(Context context,String[] items, int[] imageIds) {
+        this.context=context;
+        this.items = items;
+        ImageIds = imageIds;
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public int getCount() {
+        return items.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return items[i];
     }
 
     @Override
