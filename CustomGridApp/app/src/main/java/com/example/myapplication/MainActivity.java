0694fa11fc0cd8e6bae5e32c93d8f59483005b47
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-08-08 15:51:55 +0530

    CustomGrid

---
+++ CustomGridApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.GridView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+    String[] items={"Message","Search"};
+    int[] imageIds={R.drawable.message,R.drawable.airoplanemode};//integer array that stores id of images
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
